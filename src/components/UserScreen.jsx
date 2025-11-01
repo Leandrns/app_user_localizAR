ac0ccb067faf5789b43ff8c153da802ad641e08b
Leandrns <leandro2005souza@gmail.com>
2025-11-01 14:40:06 -0300

    style: posição do user card

--- src/components/UserScreen.jsx
+++ src/components/UserScreen.jsx
@@ -153,7 +153,6 @@ function UserScreen({
 						<i className="fa-solid fa-arrow-left"></i> Voltar
 					</button>
 				</header>
-
 				{/* Informações do Visitante */}
 				{visitante && (
 					<section className="visitor-info">
